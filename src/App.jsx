775072fc839cf2edff0979ac9f6aac2996907a3c
lchyun23 <lchyun23@gmail.com>
2024-09-19 15:47:56 +0900

    feat: 렌더링 설명

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,29 @@
+// 렌더 절차를 상세하게 본다.
+// 상태 -> 렌더 (JSX -> JSON)
+
 import '@/App.css'
-// immer import 해야 한다.
 import { produce } from 'immer'
-import { useEffect, useRef, useState } from 'react'
-
-function UsernameInput({ reference }) {
-  return (
-    <div>
-      Username : <input ref={reference} />
-    </div>
-  )
-}
-
-function PasswordInput({ reference }) {
-  const [valid, setValid] = useState({
-    maximum: false,
-    minimum: false,
-    required: false,
-  })
-
-  function changeMode(e) {
-    const input = reference.current
-    const button = e.currentTarget
-
-    if (input.type === 'password') {
-      input.type = 'text'
-      button.innerText = '감추기'
-    } else if (input.type === 'text') {
-      input.type = 'password'
-      button.innerText = '보이기'
-    }
-  }
-
-  // 유효성 검사하는 함수를 만든다.
-  function validate(e) {
-    const value = e.currentTarget.value
-    setValid(
-      produce(valid, (draft) => {
-        draft.maximum = value.length <= 10
-        draft.minimum = value.length > 5
-        draft.required = value.length > 0
-      }),
-    )
-  }
-
-  console.log('- rerendered')
-  return (
-    <div>
-      {/* input의 유효성 검사 안에서 message 쓰기  */}
-      Password :{' '}
-      <input
-        ref={reference}
-        type='password'
-        // 입력할 때마다 validation 해야 한다.
-        onChange={(e) => validate(e)}
-      />
-      <button onClick={changeMode}>보이기</button>
-      {!valid.maximum && <div style={{ color: 'red' }}>비밀번호는 10글자 넘으면 안 됨</div>}
-      {!valid.minimum && <div style={{ color: 'red' }}>비밀번호는 최소 5글자 넘어야함</div>}
-      {!valid.required && <div style={{ color: 'red' }}>비밀번호는 필수값임</div>}
-    </div>
-  )
-}
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 function App() {
-  const usernameRef = useRef(null)
-  const passwordRef = useRef(null)
+  const [value, setValue] = useState(0) // 상태가 바뀌었다.
 
-  function registration() {
-    // 회원가입 완료 버튼을 눌렀을 때 username, password 값을 콘솔로그로 찍는다.
-    // reference lifting 을 해야 한다. 부모가 값을 알아야한다.
-    console.log({
-      username: usernameRef.current.value,
-      password: passwordRef.current.value,
-    })
-  }
+  useLayoutEffect(() => {
+    console.log('2. Layout 직후 : useLayoutEffect')
+    if (value === 0) {
+      setValue(10 + Math.random() * 200)
+    }
+  }, [value])
 
-  return (
-    <section>
-      <UsernameInput reference={usernameRef} />
-      <PasswordInput reference={passwordRef} />
-      <button>회원가입 완료</button>
-    </section>
-  )
+  useEffect(() => {
+    console.log('3. Paint 직후 : useEffect')
+    if (value === 0) {
+      setValue(10 + Math.random() * 200)
+    }
+  }, [value])
+
+  console.log('1. Render : JSX -> JSON -> Virtual DOM -> Reconciliation-> Commit(DOM Mutation')
+  return <button onClick={() => setValue(0)}>value: {value}</button>
 }
 
 export default App
