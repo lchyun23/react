086a0c8855ffba66ce9d079dc064c68b8b8a4a44
lchyun23 <lchyun23@gmail.com>
2024-09-19 16:27:41 +0900

    fix: dispatch 추가

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ import { useEffect, useLayoutEffect, useReducer, useRef, useState } from 'react'
 function reducer(previous, action /* {type(수행 타입), payload(유저 파라미터) }*/) {
   switch (action.type) {
     case 'INCREASE':
-      return previous + 10
+      return previous + action.payload
       break
     case 'DECREASE':
-      return previous - 10
+      return previous - action.payload
     default:
       throw new Error('존재하지 않는 action.type입니다.')
   }
@@ -26,15 +26,15 @@ function reducer(previous, action /* {type(수행 타입), payload(유저 파라
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [count, setCount] = useReducer(reducer, 0)
+  const [count, dispatch] = useReducer(reducer, 0)
 
   return (
     <>
       <div>{count}</div>
       {/* <button onClick={() => setCount(count + 10)}>증가</button> */}
-      <button onClick={() => setCount({ type: 'INCREASE' })}>증가</button>
+      <button onClick={() => dispatch({ type: 'INCREASE', payload: 5 })}>증가</button>
       {/* <button onClick={() => setCount(count - 10)}>감소</button> */}
-      <button onClick={() => setCount({ type: 'DECREASE' })}>감소</button>
+      <button onClick={() => dispatch({ type: 'DECREASE', payload: 8 })}>감소</button>
       {/* 책임분리 느낌이네용. reducer에게 다 넘겨버리기. - redux 때문에 짚고 넘어가는 중. */}
     </>
   )
