548a5cd1ba4b6c91695504917b044bcf9c8fe10b
lchyun23 <lchyun23@gmail.com>
2024-09-19 17:21:06 +0900

    fix: context 아주 모르겠는데?

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { produce } from 'immer'
 import { createContext, useContext, useState } from 'react'
 
 function LastComponent() {
+  console.log('- A.4. Last Component')
   return <div className='component-box'>Last Component</div>
 }
 
-function ThirdComponent({ count }) {
+function ThirdComponent() {
+  const { count } = useContext(CreatedContext)
+  console.log('- A.3. Third Component')
   return (
     <div className='component-box'>
       Third Component
@@ -16,34 +19,40 @@ function ThirdComponent({ count }) {
   )
 }
 
-function SecondComponent({ count }) {
+function SecondComponent() {
+  console.log('- A.2. Second Component')
   return (
     <div className='component-box'>
       Second Component
-      <ThirdComponent count={count} />
+      <ThirdComponent />
     </div>
   )
 }
 
-function FirstComponent({ count }) {
+function FirstComponent() {
+  console.log('- A.1. First Component')
   return (
     <div className='component-box'>
       First Component
-      <SecondComponent count={count} />
+      <SecondComponent />
     </div>
   )
 }
 
-function ButtonComponent({ onClick }) {
+function ButtonComponent() {
+  const { setCount } = useContext(CreatedContext)
+  console.log('- B. Button Component')
   return (
     <div className='component-box'>
       Button Component
-      <button onClick={onClick}>증가</button>
+      <button onClick={(e) => setCount((previous) => previous + 1)}>증가</button>
     </div>
   )
 }
 
-function NonContextComponent({ count }) {
+function NonContextComponent() {
+  const { count } = useContext(CreatedContext)
+  console.log('- C. Non-Context Component')
   return (
     <div className='component-box'>
       Non-Context Component<div>{count}</div>
@@ -51,9 +60,15 @@ function NonContextComponent({ count }) {
   )
 }
 
-function App() {
+const CreatedContext = createContext({ count: -10, setCount: (e) => {} } /* Default Value, DV */)
+
+function CreatedContextProvider({ children }) {
   const [count, setCount] = useState(0)
 
+  return <CreatedContext.Provider value={{ count, setCount }}>{children}</CreatedContext.Provider>
+}
+
+function App() {
   return (
     <div
       className='section-box'
@@ -65,9 +80,11 @@ function App() {
         padding: 10,
       }}
     >
-      <FirstComponent count={count} />
-      <ButtonComponent onClick={(e) => setCount((previous) => previous + 1)} />
-      <NonContextComponent count={count} />
+      <CreatedContextProvider>
+        <FirstComponent />
+        <ButtonComponent />
+      </CreatedContextProvider>
+      <NonContextComponent />
     </div>
   )
 }
