01d847326e3aeffba7fd41b6779602cfe09b9be4
lchyun23 <lchyun23@gmail.com>
2024-09-19 16:16:02 +0900

    feat: reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
 import '@/App.css'
 import { produce } from 'immer'
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { useEffect, useLayoutEffect, useReducer, useRef, useState } from 'react'
 
-function Input({ value, onChange }) {
-  console.log('- 자식 Input 컴포넌트 rerendered (이슈 : 2번 리렌더되는걸 확인할 수 있다.)')
+/** reducer : action에 따라 상태 전이를 미리 정해놓는 것이다. 자율성 보장 안 됨.
+ *
+ * @param {*} previous = (이전) 상태
+ * @param {*} action =         상태변이 요청 (어떻게 변경할지)
+ * @returns
+ */
 
-  return (
-    <input
-      value={value}
-      onChange={(event) => {
-        const value = event.target.value
-        onChange(value)
-      }}
-    />
-  )
+function reducer(previous, action /* {type(수행 타입), payload(유저 파라미터) }*/) {
+  switch (action.type) {
+    case 'INCREASE':
+      return previous + 10
+      break
+    case 'DECREASE':
+      return previous + 10
+    default:
+      throw new Error('존재하지 않는 action.type입니다.')
+  }
 }
+// setState(previous, action) => {
+//   return previous + 1
+// }
 
 function App() {
-  const [outerValue, setOuterValue] = useState('')
-
-  console.log('- 부모 App 페이지 rerendered')
+  // const [count, setCount] = useState(0)
+  const [count, setCount] = useReducer(reducer, 0)
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Input value={outerValue} onChange={(value) => setOuterValue(value)} />
-      <input value={outerValue} onChange={(event) => setOuterValue(event.target.value)} />
-    </div>
+    <>
+      <div>{count}</div>
+      {/* <button onClick={() => setCount(count + 10)}>증가</button> */}
+      <button onClick={() => setCount({ type: 'INCREASE' })}>증가</button>
+      {/* <button onClick={() => setCount(count - 10)}>감소</button> */}
+      <button onClick={() => setCount({ type: 'DECREASE' })}>감소</button>
+      {/* 책임분리 느낌이네용. reducer에게 다 넘겨버리기. - redux 때문에 짚고 넘어가는 중. */}
+    </>
   )
 }
 
