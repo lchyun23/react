2e629769db97580a55c1f413f470348cb2f5f381
lchyun23 <lchyun23@gmail.com>
2024-09-19 15:09:06 +0900

    feat: reference 활용-몹시 신기하다

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,86 @@
 import '@/App.css'
-import { useEffect, useRef } from 'react'
+// immer import 해야 한다.
+import { produce } from 'immer'
+import { useEffect, useRef, useState } from 'react'
 
-function Header({ title }) {
+function UsernameInput({ reference }) {
   return (
-    <div style={{ position: 'sticky', top: 0, height: 60, backgroundColor: 'white' }}>
-      <div id='header-title'>{title}</div>
+    <div>
+      Username : <input ref={reference} />
     </div>
   )
 }
 
-function Title({ title }) {
-  const titleReference = useRef(null)
+function PasswordInput({ reference }) {
+  const [valid, setValid] = useState({
+    maximum: false,
+    minimum: false,
+    required: false,
+  })
 
-  useEffect(() => {
-    console.log(titleReference.current)
-    console.log(titleReference.current?.innerText)
-    // 1. Define 정의
-    const observer = new window.IntersectionObserver(([entry]) => {
-      if (!entry.intersectionRatio) {
-        document.getElementById('header-title').classList.add('show-header-title')
-      } else {
-        document.getElementById('header-title').classList.remove('show-header-title')
-      }
-    })
-    // 2. Attach 부착
-    observer.observe(titleReference.current)
-    // 3. Detach 삭제
-    return () => {
-      observer.disconnect()
+  function changeMode(e) {
+    const input = reference.current
+    const button = e.currentTarget
+
+    if (input.type === 'password') {
+      input.type = 'text'
+      button.innerText = '감추기'
+    } else if (input.type === 'text') {
+      input.type = 'password'
+      button.innerText = '보이기'
     }
-  }, [])
+  }
 
-  return <h3 ref={titleReference}>{title}</h3>
-}
+  // 유효성 검사하는 함수를 만든다.
+  function validate(e) {
+    const value = e.currentTarget.value
+    setValid(
+      produce(valid, (draft) => {
+        draft.maximum = value.length <= 10
+        draft.minimum = value.length > 5
+        draft.required = value.length > 0
+      }),
+    )
+  }
 
-function Post({ post }) {
-  return <div>{post}</div>
-}
-
-function Content({ title, post }) {
+  console.log('- rerendered')
   return (
     <div>
-      <Title title={title} />
-      <Post post={post} />
+      {/* input의 유효성 검사 안에서 message 쓰기  */}
+      Password :{' '}
+      <input
+        ref={reference}
+        type='password'
+        // 입력할 때마다 validation 해야 한다.
+        onChange={(e) => validate(e)}
+      />
+      <button onClick={changeMode}>보이기</button>
+      {!valid.maximum && <div style={{ color: 'red' }}>비밀번호는 10글자 넘으면 안 됨</div>}
+      {!valid.minimum && <div style={{ color: 'red' }}>비밀번호는 최소 5글자 넘어야함</div>}
+      {!valid.required && <div style={{ color: 'red' }}>비밀번호는 필수값임</div>}
     </div>
   )
 }
 
 function App() {
-  const example = {
-    title: 'Lorem ipsum',
-    value:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
+  const usernameRef = useRef(null)
+  const passwordRef = useRef(null)
+
+  function registration() {
+    // 회원가입 완료 버튼을 눌렀을 때 username, password 값을 콘솔로그로 찍는다.
+    // reference lifting 을 해야 한다. 부모가 값을 알아야한다.
+    console.log({
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    })
   }
+
   return (
-    <>
-      <Header title={example.title} />
-      <Content title={example.title} post={example.value} />
-    </>
+    <section>
+      <UsernameInput reference={usernameRef} />
+      <PasswordInput reference={passwordRef} />
+      <button>회원가입 완료</button>
+    </section>
   )
 }
 
