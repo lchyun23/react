8f9242cf6be4256fd5827ba6079ab8d146fc0a04
lchyun23 <lchyun23@gmail.com>
2024-09-19 17:05:38 +0900

    fix: context 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,74 @@
-// context 사용법
-
 import '@/App.css'
 import { produce } from 'immer'
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useLayoutEffect,
-  useReducer,
-  useRef,
-  useState,
-} from 'react'
+import { createContext, useContext, useState } from 'react'
 
-// Count 안에서 count를 넘기려면? - useState
-// useContext를 통해 CreatedContext 값 넘기기.
-function Count() {
-  const count = useContext(CreatedContext)
-  return <div>{count}</div>
+function LastComponent() {
+  return <div className='component-box'>Last Component</div>
 }
 
-const CreatedContext = createContext(0 /* Default Value-private context, DV */) // 0값을 제공한다. 숫자를 제공하는 context
+function ThirdComponent({ count }) {
+  return (
+    <div className='component-box'>
+      Third Component
+      <div>{count}</div>
+      <LastComponent />
+    </div>
+  )
+}
+
+function SecondComponent({ count }) {
+  return (
+    <div className='component-box'>
+      Second Component
+      <ThirdComponent count={count} />
+    </div>
+  )
+}
+
+function FirstComponent({ count }) {
+  return (
+    <div className='component-box'>
+      First Component
+      <SecondComponent count={count} />
+    </div>
+  )
+}
+
+function ButtonComponent({ onClick }) {
+  return (
+    <div className='component-box'>
+      Button Component
+      <button onClick={onClick}>증가</button>
+    </div>
+  )
+}
+
+function NonContextComponent({ count }) {
+  return (
+    <div className='component-box'>
+      Non-Context Component<div>{count}</div>
+    </div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    // provider 설정 완료. initial value.
-    <CreatedContext.Provider value={count}>
-      <Count />
-      {/* consumer */}
-      <button onClick={(e) => setCount((previous) => previous + 1)}>증가</button>
-    </CreatedContext.Provider>
+    <div
+      className='section-box'
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 16,
+        padding: 10,
+      }}
+    >
+      <FirstComponent count={count} />
+      <ButtonComponent onClick={(e) => setCount((previous) => previous + 1)} />
+      <NonContextComponent count={count} />
+    </div>
   )
 }
 
